4f8f1af70a8a0f2aaab6989a0577da89313306bf
Afganirwansyahhidayat <afganirwansyah070708@gmail.com>
2025-02-28 22:48:26 +0700

    Generator QRCode

---
+++ main.js
@@ -0,0 +1,42 @@
+var format = "png";
+async function generateQRCode() {
+  let inputText = document.getElementById("text").value;
+  if (inputText === "") {
+    // validasi jika input
+    alert("Please masukan text yang ingin diubah menjadi QR code");
+    return;
+  }
+  const response = await fetch(
+    `https://quickchart.io/qr?text=${encodeURIComponent(
+      inputText
+    )}&size=200&format=${format}`
+  ); // Fetch API
+  if (response.ok) {
+    // Generate QR code
+    const qrCodeImage = document.getElementById("qrcode");
+
+    qrCodeImage.src = URL.createObjectURL(await response.blob());
+  } else {
+    console.error("Gagal membuat kode QR");
+  }
+
+  autoClearText();
+}
+
+function autoClearText() {
+  // Clear text after generate QR code
+  document.getElementById("text").value = "";
+}
+
+function downloadQRCode() {
+  /// Download QR code
+  if (document.getElementById("qrcode").src === "") {
+    alert("Silakan buat kode QR terlebih dahulu");
+  } else {
+    const qrCodeImage = document.getElementById("qrcode");
+    const link = document.createElement("a");
+    link.href = qrCodeImage.src;
+    link.download = "qrcode.png";
+    link.click();
+  }
+}
